cc8c8aa36c968ef19868917439252b0754ca4567
Garrett <garrettleegrahamanderson@gmail.com>
2022-11-04 18:29:52 -0400

    working on styling the nav bar to open and close without losing placement on the page. added font family of roboto

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 // when user clicks on burger menu icon, toggle the ul
+    // 
+    // the header increases in height with a nice ease in animation
+    // 
 
 const menuIcon = document.querySelector('.burger-icon-menu')
 const navMenu = document.querySelector('ul')
+const header = document.querySelector('header')
 
 function openMenu() {
     navMenu.classList.toggle('open')
